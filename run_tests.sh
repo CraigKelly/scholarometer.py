a218510eee9193bc596d868c497279a9e96b80d4
Craig Kelly <craig.n.kelly@gmail.com>
2016-02-08 17:41:25 -0600

    strace helper comment for debugging

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,10 @@ cd $SCRIPT_DIR
 # Insure the cassettes directory is there
 mkdir -p .cassettes
 
+# Note that we just set logging to DEBUG and go below. If you really
+# need to capture networking stuff, you can use strace:
+# $ strace -f -e trace=network -s 10000 ./run_tests.sh
+
 # Actually startup our virtualenv and run tests with nose
 source $SCRIPT_DIR/venv/bin/activate
 nosetests --logging-level=DEBUG test.py $*
